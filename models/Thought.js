4252773813d39abb2fc8f872cad26e17ced2ea88
Norman Herrera <norman.herrera@ymail.com>
2022-07-25 21:14:41 -0700

    Thought model

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, Model } = require('mongoose');
+const dayJs = require('dayjs');
+var advancedFormat = require('dayjs/plugin/advancedFormat');
+dayJs.extend(advancedFormat);
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema ({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 300,
+    },
+    createdAt: {
+        type: Date,
+        default: dayJs(),
+        get: (date) => dayJs(date).format("MMM Do YYYY [at] h:mm A"),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+}, {
+    toJSON: {
+        getters: true,
+    }, 
+    id: false,
+});
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+
+const Thought = Model('thought', thoughtSchema);
+
+module.exports = Thought
